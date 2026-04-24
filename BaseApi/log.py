d763e5e5590d775fd40d1f061894d4a6a04f0a40
sj-chen <1095668361@qq.com>
2026-04-24 13:50:58 +0800

    2

--- BaseApi/log.py
+++ BaseApi/log.py
@@ -1,12 +1,23 @@
 import logging
+import os
 import sys
+from datetime import datetime
+from pathlib import Path
+
 import pytest
 
-from common.file import ROOT_PATH
 
 
 @pytest.fixture(scope='session')
 def setup_logging():
+    BASE_DIR = Path(__file__).resolve().parent.parent  # BaseApi 的上层目录
+    LOG_DIR = BASE_DIR / "log"
+    LOG_DIR.mkdir(exist_ok=True)  # 确保目录存在
+    # 2. 生成带日期的文件名，例如 api_test_2026-04-24.log
+    today_str = datetime.now().strftime("%Y-%m-%d")
+    log_filename = f"api_test_{today_str}.log"
+    log_path = LOG_DIR / log_filename
+
     logging.getLogger("urllib3").setLevel(logging.DEBUG)
     logger = logging.getLogger("urllib3")
     logger.setLevel(logging.DEBUG)
@@ -17,7 +28,7 @@ def setup_logging():
     console_handler.setFormatter(formatter)
     logger.addHandler(console_handler)
 
-    file_handler = logging.FileHandler('D://PycharmProjects/ds/log/api_test.log')
+    file_handler = logging.FileHandler(log_path)
     file_handler.setLevel(logging.DEBUG)
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
     file_handler.setFormatter(formatter)
